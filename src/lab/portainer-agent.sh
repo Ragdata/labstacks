0adc40a1c8286bd6a449f962e98bf9bd561326e7
Ragdata <ragdata@users.noreply.github.com>
2025-12-24 11:38:42 +1000

    add container services scripts

---
+++ src/lab/portainer-agent.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+####################################################################
+# Author:       Ragdata
+# Date:         24/12/2025
+# License:      MIT License
+# Copyright:    Copyright © 2025 Redeyed Technologies
+####################################################################
+# DEPLOY AGENT CONTAINER
+####################################################################
+docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  --security_opt=apparmor:unconfined \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  -v /:/host \
+  portainer/agent:latest
