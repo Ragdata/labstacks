5ef2ce9702caa1dc86e7f590016860a8e6edba6c
Ragdata <ragdata@users.noreply.github.com>
2025-12-29 19:55:07 +1000

    refactor portainer-agent.sh

--- src/scr/portainer-agent.sh
+++ src/scr/portainer-agent.sh
@@ -11,7 +11,6 @@ docker run -d \
   -p 9001:9001 \
   --name portainer_agent \
   --restart=always \
-  --security_opt=apparmor:unconfined \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /var/lib/docker/volumes:/var/lib/docker/volumes \
   -v /:/host \
