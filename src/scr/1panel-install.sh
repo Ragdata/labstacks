f1c60600b9a9523d02071bcad45820608aeaa32d
Ragdata <ragdata@users.noreply.github.com>
2025-12-29 20:05:14 +1000

    add scripts

---
+++ src/scr/1panel-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+####################################################################
+# Author:       Ragdata
+# Date:         29/12/2025
+# License:      MIT License
+# Copyright:    Copyright © 2025 Redeyed Technologies
+####################################################################
+# INSTALL 1Panel
+####################################################################
+curl -sSL https://resource.1panel.pro/quick_start.sh -o quick_start.sh && bash quick_start.sh
---
+++ src/scr/lazydocker-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+####################################################################
+# Author:       Ragdata
+# Date:         29/12/2025
+# License:      MIT License
+# Copyright:    Copyright © 2025 Redeyed Technologies
+####################################################################
+# INSTALL LAZYDOCKER
+####################################################################
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
