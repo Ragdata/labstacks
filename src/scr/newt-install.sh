a517b48a9e37bea591dc84ae5d442761ffb83315
Ragdata <ragdata@users.noreply.github.com>
2026-01-16 04:24:03 +1000

    add new install script

---
+++ src/scr/newt-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+####################################################################
+# Author:       Ragdata
+# Date:         14/01/2026
+# License:      MIT License
+# Copyright:    Copyright © 2026 Redeyed Technologies
+####################################################################
+# INSTALL NEWT
+####################################################################
+curl -fsSL https://static.pangolin.net/get-newt.sh | bash
