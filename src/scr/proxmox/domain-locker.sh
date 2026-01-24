bb4e613888ad4ba7c57e9c59d26fb0e19528c721
Ragdata <ragdata@users.noreply.github.com>
2026-01-24 10:16:10 +1000

    add proxmox scripts

---
+++ src/scr/proxmox/domain-locker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+####################################################################
+# Author:       Ragdata
+# Date:         20/01/2026
+# License:      MIT License
+# Copyright:    Copyright © 2025 Redeyed Technologies
+####################################################################
+# INSTALL DOMAIN-LOCKER
+####################################################################
+# Location of config file: /opt/domain-locker.env
+# Show DB Credentials: cat ~/Domain-Locker.creds
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/ct/domain-locker.sh)"
